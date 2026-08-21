8b228ebd2c2237955401b81239f11906089a4636
Kemal Aydın <83170943+kemallaydn@users.noreply.github.com>
2026-08-21 18:40:19 +0300

    test(logging): compile logger contracts for issue #7

--- src/fac_lpr_engine.cpp
+++ src/fac_lpr_engine.cpp
@@ -1,5 +1,6 @@
 #include <fac_lpr/application/config.hpp>
 #include <fac_lpr/application/error.hpp>
+#include <fac_lpr/application/logging.hpp>
 #include <fac_lpr/application/providers.hpp>
 #include <fac_lpr/domain/recognition.hpp>
 #include <fac_lpr/fac_lpr_engine.hpp>
@@ -17,6 +18,7 @@ static_assert(std::is_move_constructible_v<domain::PlateRecognitionResult>);
 static_assert(std::has_virtual_destructor_v<application::IPlateDetector>);
 static_assert(std::has_virtual_destructor_v<application::IPlateRecognizer>);
 static_assert(std::has_virtual_destructor_v<application::IDecisionPolicy>);
+static_assert(std::has_virtual_destructor_v<application::ILogger>);
 static_assert(application::EngineConfig{}.decision.fail_closed);
 static_assert(std::is_base_of_v<std::runtime_error, application::EngineError>);
 static_assert(std::is_base_of_v<application::EngineError, application::ConfigurationError>);
