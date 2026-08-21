9dda4027f56df7a3ea232d14ade3ec48d9c8c551
Kemal Aydın <83170943+kemallaydn@users.noreply.github.com>
2026-08-21 18:34:34 +0300

    test(domain): compile domain value contracts for issue #3

--- src/fac_lpr_engine.cpp
+++ src/fac_lpr_engine.cpp
@@ -1,7 +1,14 @@
+#include <fac_lpr/domain/recognition.hpp>
 #include <fac_lpr/fac_lpr_engine.hpp>
+
+#include <type_traits>
 
 namespace fac_lpr {
 
 static_assert(!Engine::name().empty());
+static_assert(std::is_copy_constructible_v<domain::BoundingBox>);
+static_assert(std::is_copy_constructible_v<domain::Detection>);
+static_assert(std::is_copy_constructible_v<domain::PlateCandidate>);
+static_assert(std::is_move_constructible_v<domain::PlateRecognitionResult>);
 
 } // namespace fac_lpr
