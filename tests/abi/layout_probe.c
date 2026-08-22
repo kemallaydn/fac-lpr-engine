550484c3d5c74dbf8ca0a7900c3ab78f5c9d2eab
Kemal Aydın <83170943+kemallaydn@users.noreply.github.com>
2026-08-23 00:42:17 +0300

    abi: make layout probe portable to MSVC

--- tests/abi/layout_probe.c
+++ tests/abi/layout_probe.c
@@ -1,10 +1,15 @@
 #include <fac_lpr/fac_lpr_engine.h>
 
-#include <stdalign.h>
 #include <stdio.h>
 
+#if defined(_MSC_VER)
+#define ABI_ALIGNOF(type) __alignof(type)
+#else
+#define ABI_ALIGNOF(type) _Alignof(type)
+#endif
+
 #define ENTRY(type, comma) \
-    printf("  \"%s\": {\"size\": %zu, \"align\": %zu}%s\n", #type, sizeof(type), alignof(type), comma)
+    printf("  \"%s\": {\"size\": %zu, \"align\": %zu}%s\n", #type, sizeof(type), (size_t)ABI_ALIGNOF(type), comma)
 
 int main(void) {
     puts("{");
